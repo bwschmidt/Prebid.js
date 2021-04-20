635c3afdb6e10f834113772e412745419e2638d5
Brian Schmidt <brian.schmidt@openx.com>
2021-04-19 17:42:55 -0700

    remove swan eid

--- modules/userId/eids.js
+++ modules/userId/eids.js
@@ -120,12 +120,6 @@ const USER_IDS_CONFIG = {
     atype: 1
   },
 
-  // swanId
-  'swanId': {
-    source: 'swan.io',
-    atype: 1
-  },
-
   // sharedid
   'sharedid': {
     source: 'sharedid.org',
